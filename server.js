27320b2c2cf20e912b734029c2e1fdf5227185ed
hectorgarciatw <dreamallica@gmail.com>
2020-06-19 20:41:16 -0300

    Agragando UI Kit y los módulos Bootstrap, Path y Jquery

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const mysql = require('mysql');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Bootstrap requirements
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var con = mysql.createConnection({
@@ -27,7 +37,7 @@ app.set('view engine','ejs');
 
 //Rutas
 app.get('/',(req,res) =>{
-  res.end('Hola mundo de nuevo!');
+  res.render('dashboard');
 });
 
 //Fin Rutas
