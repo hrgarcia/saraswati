6007a38633a1b2e9e1df66990d083d869ec908ed
MarcosPiccotto <marcospiccotto12@gmail.com>
2020-06-29 12:03:11 -0300

    Hicimos la validacion de datos del login

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "saraswatidb"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "saraswatidb"
 });
 
 con.connect(function(error){
-  if(error) throw error;
-  console.log("Conectado a la DB");
+	if(error) throw error;
+	console.log("Conectado a la DB");
 });
 
 //Configuración
@@ -37,27 +37,27 @@ app.set('view engine','ejs');
 
 //Rutas
 app.get('/',(req,res) =>{
-  res.render('login.ejs');
+	res.render('login.ejs');
 });
 
 app.get('/dashboard',(req,res) =>{
-  res.render('dashboard.ejs');
+	res.render('dashboard.ejs');
 });
 
 app.get('/agregarProfesor',(req,res) =>{
-  res.render('agregarProfesor.ejs');
+	res.render('agregarProfesor.ejs');
 });
 
 app.get('/listarProfesores',(req,res) =>{
-  let query = "SELECT * FROM profesor";
-  con.query(query,function(error,rows,fields){
+	let query = "SELECT * FROM profesor";
+	con.query(query,function(error,rows,fields){
     if(error) throw error;
     res.render('listarProfesores.ejs',{title:"Profesores",datos:rows});
-  });
+	});
 });
 
 app.post('/createTeacher', urlencodedParser, function (req, res) {
-  let user = req.body.user;
+	let user = req.body.user;
 	let name = req.body.name;
 	let lastName = req.body.lastName;
 	let dni = req.body.dni;
@@ -66,24 +66,35 @@ app.post('/createTeacher', urlencodedParser, function (req, res) {
 	let gender = req.body.gender;
 	let birth = req.body.birth;
 	let entry = req.body.entry;
-  let state = req.body.state;
-  
+	let state = req.body.state;
+
 	let query = "INSERT INTO profesor (usuario,nombre,apellido,dni,telefono,email,genero,nacimiento,ingreso,estado) VALUES ('"+user+"','"+name+"','"+lastName+"','"+dni+"','"+telephone+"','"+email+"','"+gender+"','"+birth+"','"+entry+"','"+state+"');";
 	con.query(query, function(error,rows,fields){
     if(error) throw error;
     //verificar porque datos no funciona si recargo la pagina listarProfesores luego de hacer un insert
     //res.render('listarProfesores.ejs');
-		res.render('dashboard.ejs');
-	})
-})
+	res.render('dashboard.ejs');
+	});
+});
 
 app.post('/loginValidation', urlencodedParser,function(req,res){
-  console.log("hola");
-  //validar utilizando select y if
-  res.render('dashboard.ejs');
-})
+    let user = req.body.user;
+	let password = req.body.password;
+
+    //pusimos nombre porque no existe un atributo contraseña
+	let query = "SELECT usuario,nombre From profesor";
+	con.query(query, function(error,rows,fields){
+    if(error) throw error;
+    for (let index = 0; index < rows.length; index++) {
+		if(rows[index]['usuario'] == user && rows[index]['nombre'] == password){
+            res.render('dashboard.ejs');
+        } 
+    }
+    res.render('login.ejs');
+    });
+});
 //Fin Rutas
 
-app.listen(2500,() =>{
-  console.log("El servidor corriendo en el puerto 2500");
+app.listen(2500,() => {
+	console.log("El servidor corriendo en el puerto 2500");
 });
