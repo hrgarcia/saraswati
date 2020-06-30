bc2cfbfca05a04b88263930f04cff33c309ef661
nahuelonnn <nahupetrocelli@gmail.com>
2020-06-29 21:04:23 -0300

    saque el codigo viejo

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser')
 
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
