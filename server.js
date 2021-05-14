d0cdaa0fcd3cc165b1f6e9aea41324b532ea9508
marcos <marcospiccotto12@gmail.com>
2021-05-14 20:55:14 -0300

    Validacion al crear aprendizajes por archivo completo

--- server.js
+++ server.js
@@ -377,7 +377,7 @@ app.post("/cargarAprendizaje", aprendizajesExcel, (req, res, next) => {
     let trimester = req.body.trimester;
     let idSubject = req.body.idSubject;
 
-    //teacherFunctions.loadLearnings(req.file.path, con, typeOFile, trimester, idSubject);
+    teacherFunctions.loadLearnings(req.file.path, con, typeOFile, trimester, idSubject);
     res.redirect("/dashboard");
 });
 
